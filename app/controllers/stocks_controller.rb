3a361f8f5daaaf48e6559f551cb4f1185a9f5251
Kevin Kabore <kevin.s.kabore@gmail.com>
2016-07-28 11:39:08 -0700

    Adds some styling

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,14 +8,4 @@ class StocksController < ApplicationController
     @stocks = Stock.all
   end
 
-  def create
-    # (buy button? Or in index?) prompts user to add quantity and purchase price
-    # @portfolio_item  = Portfolio.new(params.require())
-    # if @portfolio_item.save
-    #   redirect_to "stocks#index"
-    # else
-    #   # ?
-    # end
-
-  end
 end
